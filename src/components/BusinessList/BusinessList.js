6676b1e4fe30b46640031c4d0cfb367a1dd9a5fa
David Brown <david.brown@windowslive.com>
2019-05-25 15:11:18 +1200

    with yelp api fetch

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,11 +7,11 @@ class BusinessList extends React.Component {
     return (
       <div className="BusinessList" >
       {this.props.businesses.map(business => {
-        return <Business business={business}/>
+        return <Business key={business.id} business={business}/>
       })}
       </div>
     );
   }
-};
+}
 
 export default BusinessList;
